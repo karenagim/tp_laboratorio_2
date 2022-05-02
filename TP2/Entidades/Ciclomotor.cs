fbf71be09b2c4230302e4c06d8af621fd4d4d3dd
Karen Gimenez <24karengimenez@gmail.com>
2022-05-02 03:40:56 -0300

    Entrega trabajo practico n2

---
+++ TP2/Entidades/Ciclomotor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color) : base(marca, chasis, color)
+        {
+        }
+
+
+        /// <summary>
+        /// Propiedad de Ciclomotor, por defecto es 'Chico'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+               return ETamanio.Chico;
+            }
+        }
+
+     /// <summary>
+     /// Muestra por pantalla los datos de ciclomotor
+     /// </summary>
+     /// <returns></returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CICLOMOTOR");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("TAMAÑO : {0}\n", this.Tamanio);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TP2/Entidades/Suv.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Suv:Vehiculo
+    {
+        public Suv(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+
+        }  
+        
+        
+        /// <summary>
+        /// Propiedad protegida de SUV, tamanio por defecto  'Grande'
+        /// </summary>   
+        
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Grande;
+            }
+        }
+
+        /// <summary>
+        /// Muestra por pantalla los datos de un SUV
+        /// </summary>
+        /// <returns></returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SUV");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("TAMAÑO: {0}\n", this.Tamanio);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
